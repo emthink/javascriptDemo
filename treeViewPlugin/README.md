7d9e0fac0cc2818872b6dedcfcf2e388a5ca2a8b
codingplayboy <codingfun@163.com>
2016-01-10 00:17:57 +0800

    update the plugin
    update the plugin

--- treeViewPlugin/README.md
+++ treeViewPlugin/README.md
@@ -27,8 +27,9 @@ treeViewPlugin，一个可以生成任意多级树列表的js插件，使用了j
 	return {
 		initModule: initModule,  //初始化树列表
 		addParents: addParents,  //给树列表添加父级菜单
-        	addChild: addChild,  //给树列表添加子级菜单
-        	addChildNode: addChildNode  //给树列表添加无子级的子级菜单
+        addChild: addChild,  //给树列表添加子级菜单
+        addChildNode: addChildNode,  //
+		setData: setData //给插件树设置值
 	};
 ```
 
@@ -47,12 +48,12 @@ treeViewPlugin，一个可以生成任意多级树列表的js插件，使用了j
 	};
 ```
 
-#### 添加父级菜单
+#### 添加无父级有子级菜单
 
 ```
 
 	/**
-	 * [addParents 添加父级菜单
+	 * [addParents 添加无父级有子级菜单
 	 * @param {[Array]} _data [菜单数据]
 	 * [{
 	 * 		id: 菜单id,
@@ -65,12 +66,30 @@ treeViewPlugin，一个可以生成任意多级树列表的js插件，使用了j
 	};
 ```
 
-#### 添加子级菜单
+#### 添加无父级无子级菜单
 
 ```
 
 	/**
-     * [addChild 添加子级菜单]
+	 * [addOne 添加无父级无子级菜单
+	 * @param {[Array]} _data [菜单数据]
+	 * [{
+	 * 		id: 菜单id,
+	 * 		name: 菜单显示值
+	 * 
+	 * }]
+	 */
+	addOne = function(_data) {
+		//具体实现
+	};
+```
+
+#### 添加有父级有子级菜单
+
+```
+
+	/**
+     * [addChild 添加有父级有子级菜单]
      * @param {[Array]} _data [菜单数据]
      * [{
      * 		id: 菜单id,
@@ -83,12 +102,12 @@ treeViewPlugin，一个可以生成任意多级树列表的js插件，使用了j
     };
 ```
 
-#### 添加无子级的菜单
+#### 添加有父级无子级的菜单
 
 ```
 
 	/**
-     * [addChildNode 添加无子级子级菜单]
+     * [addChildNode 添加有父级无子级菜单]
      * @param {[Array]} _data [菜单数据]
      * [{
      * 		id: 菜单id,
@@ -101,11 +120,41 @@ treeViewPlugin，一个可以生成任意多级树列表的js插件，使用了j
      };
 ```
 
+#### setData为树列表填充值
+
+```
+
+	/**
+     * [setData 为树列表填充值]
+     * @param {[type]} data [Array]
+     */
+	setData = function(data) {
+		var result = {
+            self: [],  //无父级无子级树菜单集合
+            parents: [],  //无父级有子级树菜单集合
+            children: [],  //填充有父级有子级树菜单集合
+            members: [] //填充只有父级无子级树菜单集合
+        };
+        //具体实现...
+        for (var i = 0, _len = data.length; i < _len; i++) {
+            id_arr.push(data[i].id);
+            data[i]['has-child-here-flag'] = 0; //有无子级标志，默认0为无
+            data[i]['has-parent-here-flag'] = 0; //有无父级标志，默认0为无
+        }
+        //...
+		addParents(result.parents);//填充无父级有子级树菜单
+        addOne(result.self);//填充无父级无子级树菜单
+        addChildren(result.children);//填充有父级有子级树菜单
+        addChildNode(result.members);//填充只有父级无子级树菜单
+        return result;
+	};
+```
+
 ## demo展示
 
 ```
 
-	<!DOCTYPE html>
+		<!DOCTYPE html>
 	<html lang="en">
 	<head>
 		<meta charset="UTF-8">
@@ -131,76 +180,82 @@ treeViewPlugin，一个可以生成任意多级树列表的js插件，使用了j
 		<div class="container">
 			<div class="treeviewCont"></div>
 		</div>
+		<span id="busuanzi_container_site_pv">
+	    	本站总访问量<span id="busuanzi_value_site_pv"></span>次
+		</span>
+		<script src="./js/jquery-1.11.3.js"></script>
+		<script src="./js/js-treeview-plugin.js"></script>
+		<script async src="https://dn-lbstatics.qbox.me/busuanzi/2.3/busuanzi.pure.mini.js"></script>
+
 		<script>
 			$(function() {
 				var tree = $('.treeviewCont');
+	            var data = [
+	                {
+	                    name: '一级菜单1',
+	                    id: 'id_01'
+	                },
+	                {
+	                    name: '一级菜单2',
+	                    id: 'id_02'
+	                },
+	                {
+	                    name: '一级菜单3',
+	                    id: 'id_03'
+	                },
+	                {
+	                    name: '一级菜单4',
+	                    id: 'id_04'
+	                },
+	                {
+	                    pId: 'id_01',
+	                    name: '二级菜单1001',
+	                    id: 'id_01_001'
+	                },
+	                {
+	                    pId: 'id_01',
+	                    name: '二级菜单102',
+	                    id: 'id_01_02'
+	                },
+	                {
+	                    pId: 'id_03',
+	                    name: '二级菜单3001',
+	                    id: 'id_03_001'
+	                },
+	                {
+	                    pId: 'id_04',
+	                    name: '二级菜单4001',
+	                    id: 'id_04_001'
+	                },	   
+	                {
+	                    pId: 'id_03',
+	                    name: '二级菜单3002',
+	                    id: 'id_03_002'
+	                },
+	                {
+	                    pId: 'id_04',
+	                    name: '二级菜单4002',
+	                    id: 'id_04_002'
+	                },
+	                {
+	                    pId: 'id_01_02',
+	                    name: '三级菜单12001',
+	                    id: 'id_01_02_001'
+	                },
+	                {
+	                    pId: 'id_00',
+	                    name: '一级菜单5',
+	                    id: 'id_05'
+	                }
+	            ];
 				treeViewPlugin.initModule(tree);
-				treeViewPlugin.addParents([
-		                {
-		                    name: 'Betty',
-		                    id: 'id_01'
-		                },
-		                {
-		                    name: 'Mike',
-		                    id: 'id_02'
-		                },
-		                {
-		                    name: 'Pebbles',
-		                    id: 'id_03'
-		                },
-		                {
-		                    name: 'Wilma',
-		                    id: 'id_04'
-		                }
-		            ]);
-		            treeViewPlugin.addChild([
-		                {
-		                    pId: 'id_01',
-		                    name: 'Betty',
-		                    id: 'id_01_01'
-		                },
-		                {
-		                    pId: 'id_01',
-		                    name: 'Mike',
-		                    id: 'id_01_02'
-		                },
-		                {
-		                    pId: 'id_03',
-		                    name: 'Pebbles',
-		                    id: 'id_03_01'
-		                },
-		                {
-		                    pId: 'id_04',
-		                    name: 'Wilma',
-		                    id: 'id_04_01'
-		                }
-		            ]);
-		            treeViewPlugin.addChildNode([
-		                {
-		                    pId: 'id_01',
-		                    name: 'Btrey',
-		                    id: 'id_01_001'
-		                },
-		                {
-		                    pId: 'id_01',
-		                    name: 'Hans',
-		                    id: 'id_01_002'
-		                },
-		                {
-		                    pId: 'id_03',
-		                    name: 'wekous',
-		                    id: 'id_03_001'
-		                },
-		                {
-		                    pId: 'id_04',
-		                    name: 'Wimaka',
-		                    id: 'id_04_001'
-		                }
-		            ]);
+	            treeViewPlugin.setData(data);
+				
 			});
 		</script>
 	</body>
 	</html>
+
 ```
 
 **此插件只是初步实现，更多功能待完善，实际使用可根据实际需求做相应修改，欢迎大家一起分享、交流、学习。**
--- treeViewPlugin/js/js-treeview-plugin.js
+++ treeViewPlugin/js/js-treeview-plugin.js
@@ -3,8 +3,8 @@
  * @description [make a dynamic tree with arbitrary child nodes.]
  * @author [惊鸿三世](http://blog.codingplayboy.com)
  * @{@link :https://github.com/codingplayboy/}
- * @version [0.1.0]
- * @date: 2016.01.07
+ * @version [0.1.1]
+ * @date: 2016.01.07,01.10
  * 
  */
 
@@ -28,7 +28,7 @@ var treeViewPlugin = (function() {
 		$append_target: null,
 	},
 	jqueryMap = {},
-	setJqueryMap, addGroup, configModule, initModule;
+	setJqueryMap, addGroup, addParents, addChildren, addChildNode, configModule, initModule, setData;
 
 	setJqueryMap = function() {
 		var $append_target = stateMap.$append_target,
@@ -69,18 +69,38 @@ var treeViewPlugin = (function() {
                 + '</div>'
                 + '</div>';
         });
-        if (!list_html) {
-            list_html = String()
-                + '<div class="spa-treeview-group-list-note">'
-                + '暂无数据<br><br>'
-                + ''
-                + '</div>';
+        // if (!list_html) {
+        //     list_html = String()
+        //         + '<div class="spa-treeview-group-list-note">'
+        //         + '暂无数据<br><br>'
+        //         + ''
+        //         + '</div>';
+        // }
+        jqueryMap.$parentFir.children('.noUse').before(list_html);
+    };
+
+    addOne = function(_data) {
+        var list_html = String();
+        if (!_data) {
+            _data = [];
         }
-        jqueryMap.$parentFir.html(list_html);
+        $(_data).each(function(index) {
+            list_html += '<div class="spa-treeview-anchor spa-treeview-group-list-name'
+                + '" data-id="' + _data[index].id
+                + '" data-group="' + _data[index].name
+                + '" title="' + _data[index].name + '" style="margin-bottom:6px;">'
+                + '<span class="childIcon"></span><a href="javascript:void(0);" class="spa-data-item">'
+                + _data[index].name
+                + '</a><span class="icon-nochecked"></span><div class="spa-treeview-list-contacts">'
+                + '<span class="last"></span>'
+                + '</div>'
+                + '</div>';
+        });
+        jqueryMap.$parentFir.children('.noUse').before(list_html);
     };
 
     /**
-     * [addChild 添加子级菜单]
+     * [addChildren 添加子级菜单]
      * @param {[Array]} _data [菜单数据]
      * [{
      * 		id: 菜单id,
@@ -88,7 +108,7 @@ var treeViewPlugin = (function() {
      * 		pId: 父级菜单id
      * }]
      */
-    addChild = function(_data) {
+    addChildren = function(_data) {
         var list_html = String();
         var $parentSec = jqueryMap.$parent.find('.spa-treeview-group-list-name');
         if (!_data) {
@@ -129,8 +149,9 @@ var treeViewPlugin = (function() {
         var $parentSec = jqueryMap.$parent.find('.spa-treeview-group-list-name');
         $(_data).each(function(id) {
                 $parentSec.each(function(idx){
-                    if (_data[id].pId.indexOf($($parentSec[idx]).attr('data-id')) >= 0) {
-                        list_html = '<div class="spa-treeview-anchor spa-treeview-contacts-list"'
+                    if (_data[id].pId == $($parentSec[idx]).attr('data-id')) {
+                        console.log(_data[id].pId, $($parentSec[idx]).attr('data-id'));
+                        list_html = '<div class="spa-treeview-anchor spa-treeview-contacts-list-name"'
                         	+ '" data-id="' + _data[id].id
 	                        + '" data-contact="' + _data[id].name
 	                        + '" title="' + _data[id].name + '">'
@@ -170,8 +191,8 @@ var treeViewPlugin = (function() {
         var n = $(this).next();
         if (n.hasClass('icon-checked') || n.hasClass('icon-nochecked')) {
             n.hasClass("icon-checked")
-                ? n.removeClass("icon-checked").addClass("icon-nochecked")
-                : n.removeClass("icon-nochecked").addClass("icon-checked");
+                ? n.removeClass("icon-checked").addClass("icon-nochecked") && n.parents('.spa-treeview-anchor').children('.icon-checked').removeClass("icon-checked").addClass("icon-nochecked")
+                : n.removeClass("icon-nochecked").addClass("icon-checked") && n.parents('.spa-treeview-anchor').children('.icon-nochecked').removeClass("icon-nochecked").addClass("icon-checked");
         }
         return;
     };
@@ -190,14 +211,72 @@ var treeViewPlugin = (function() {
 		$append_target.append(configMap.main_html);
 		stateMap.$append_target = $append_target;
 		setJqueryMap();
+        jqueryMap.$parentFir.html('<span style="display:none;" class="noUse"></span>')
 		return true;
 	};
 
+    /**
+     * [setData 为树列表填充值]
+     * @param {[type]} data [Array]
+     */
+	setData = function(data) {
+        var result = {
+            self: [],
+            parents: [],
+            children: [],
+            members: []
+        };
+        var _len = data.length;
+        var id_arr = [];
+        var _id = '';
+        var k = 0;
+        for (var i = 0, _len = data.length; i < _len; i++) {
+            id_arr.push(data[i].id);
+            data[i]['has-child-here-flag'] = 0;
+            data[i]['has-parent-here-flag'] = 0;
+        }
+        for (var j = 0, _len = data.length; j < _len; j++) {
+            var _data = data[j];
+            _id = _data.parentid || _data.pId || '';
+            k = id_arr.length;
+            while (k--) {
+                if (_id == id_arr[k]) {
+                    data[k]['has-child-here-flag']++;
+                    _data['has-parent-here-flag']++;
+                }
+            }
+        }
+        for (var m = 0, _len = data.length; m < _len; m++) {
+            var _res = data[m];
+            if (_res['has-child-here-flag'] && !_res['has-parent-here-flag']) {
+                result.parents.push(_res);
+            }
+            if (_res['has-parent-here-flag'] && _res['has-child-here-flag']) {
+                result.children.push(_res);
+            }
+            if (!_res['has-child-here-flag'] && _res['has-parent-here-flag']) {
+                result.members.push(_res);
+            }else if (!_res['has-child-here-flag'] && !_res['has-parent-here-flag']) {
+                result.self.push(_res);
+            }
+        }
+        console.log(result);
+        console.log(data);
+
+        addParents(result.parents);//填充无父级树菜单
+        addOne(result.self);//填充无父级无子级树菜单
+        addChildren(result.children);//填充有父级有子级树菜单
+        addChildNode(result.members);//填充只有父级无子级树菜单
+        return result;
+    };
+    
 	return {
 		configModule: configModule,
 		initModule: initModule,
 		addParents: addParents,
-        addChild: addChild,
-        addChildNode: addChildNode
+        addOne: addOne,
+        addChildren: addChildren,
+        addChildNode: addChildNode,
+        setData: setData
 	};
  }());
